8fb608e66161b60a4370057ba37433b53df7fd77
r351574nc3 <r351574nc3@gmail.com>
2015-01-14 17:33:00 -0700

    Cleaning up debugging

--- js/collection-views/generator.img.collection-view.js
+++ js/collection-views/generator.img.collection-view.js
@@ -93,7 +93,6 @@ define(['jquery', 'underscore', 'backbone', 'dropzone', 'util/authenticate', 'im
 				});
 			}
 			
-			console.log('Imgs', this.collection.url);
 			this.dropzone_form = new Dropzone('#' + this.dropzoneId, {
 				url: this.collection.url,
 				method: "POST",
